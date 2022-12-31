c5673347e81551e524e078e0e9fa6dadb3a55127
AegisofSins <113641383+AegisofSins@users.noreply.github.com>
2022-12-30 18:24:36 -0800

    Add mainGame

--- script.js
+++ script.js
@@ -3,20 +3,45 @@
 //User choice gets compared to computer choice (playerSelection vs compSelection) 
 // if playerSelection = Rock && compSelection = Paper then return alert('You won') else alert('you lost')
 
-const gameArray = ['rock', 'paper', 'scissor'];
+const gameArray = ['ROCK', 'PAPER', 'SCISSORS'];
+let compChoice = compSel();
+let playerChoice = playerSel();
+let result = playRound();
+
+//Computer Selection that is random using the above declared array ///////////////////////////////////////////////
 
 function compSel() {  //Generates random output for computer between Rock, Paper, and Scissor
     let compChoice = Math.floor(Math.random()*gameArray.length);
+    console.log(gameArray[compChoice]);
     return (gameArray[compChoice]);
 }
 
+//Player Selection using a prompt and assigning it to a variable //////////////////////////////////////////////////
+
 function playerSel () {
     let playerChoice = prompt('Choose Rock, Paper, or Scissors').toUpperCase(); //Log players choice in Uppercase 
-    if (playerChoice == 'ROCK' || playerChoice == 'PAPER' || playerChoice == 'SCISSORS' ) {
-        return playerChoice;
+    if (playerChoice == 'ROCK' || playerChoice == 'PAPER' || playerChoice == 'SCISSORS' ) { //Verifies that user entry is True
     }
     else {
         alert ('Not a valid choice')
     }
+    return playerChoice;
 } 
+
+//Program that compares both input and returns the winner 
+
+function playRound() {
+    if (compChoice === playerChoice){
+        alert ('Tie!') }
+    else if (compChoice == 'ROCK' && playerChoice == 'SCISSORS' || 
+            compChoice == 'PAPER' && playerChoice == 'ROCK' || 
+            compChoice == 'SCISSORS' && playerChoice == 'PAPER') {
+                alert ('Computer won')
+            }
+    else {
+        alert ('You won!')
+    }
+
+}
+
         
